47453e21c98d6ad03c7c2f272337d68025dc32a1
haroldtreen <haroldtreen@gmail.com>
2014-03-08 23:06:18 -0500

    Removed unnecessary provider.

--- src/main/java/com/appattack/phoneaddict/InjectionModule.java
+++ src/main/java/com/appattack/phoneaddict/InjectionModule.java
@@ -1,16 +1,11 @@
 package com.appattack.phoneaddict;
 
 
-import com.appattack.phoneaddict.service.WakeService;
-import com.appattack.phoneaddict.service.WakeServiceProvider;
-import com.appattack.phoneaddict.tracker.WakeTracker;
-import com.appattack.phoneaddict.tracker.WakeTrackerProvider;
 import com.google.inject.AbstractModule;
 
 public class InjectionModule extends AbstractModule {
 
     protected void configure(){
-        bind(WakeTracker.class).toProvider(WakeTrackerProvider.class);
-        bind(WakeService.class).toProvider(WakeServiceProvider.class);
+        //bind
     }
 }
