def1235dbd954a2935c77f8301a71523e7c66626
haroldtreen <haroldtreen@gmail.com>
2014-03-13 23:01:45 -0400

    Updated main activity tests.

--- src/test/java/com/appattack/phoneaddict/TestInjectionModule.java
+++ src/test/java/com/appattack/phoneaddict/TestInjectionModule.java
@@ -1,11 +1,14 @@
 package com.appattack.phoneaddict;
 
+import com.appattack.phoneaddict.services.ServiceUtilities;
 import com.google.inject.AbstractModule;
+
+import org.mockito.Mockito;
 
 public class TestInjectionModule extends AbstractModule{
 
     @Override
     protected void configure(){
-        //bind stuff
+        bind(ServiceUtilities.class).toInstance(Mockito.mock(ServiceUtilities.class));
     }
 }
--- src/test/java/com/appattack/phoneaddict/activities/MainActivityTest.java
+++ src/test/java/com/appattack/phoneaddict/activities/MainActivityTest.java
@@ -1,23 +1,34 @@
 package com.appattack.phoneaddict.activities;
 
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.widget.ToggleButton;
 
+import com.appattack.phoneaddict.R;
 import com.appattack.phoneaddict.RobolectricTestRunnerWithInjection;
+import com.appattack.phoneaddict.services.ServiceUtilities;
+import com.appattack.phoneaddict.services.WakeService;
+import com.google.inject.Inject;
+
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.Robolectric;
 import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.util.ActivityController;
 
-import static org.junit.Assert.assertArrayEquals;
+import static junit.framework.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunnerWithInjection.class)
 public class MainActivityTest {
+
+    ToggleButton serviceButton;
+    @Inject ServiceUtilities svcUtilities;
 
     private ActivityController<MainActivity> controller;
     private MainActivity activity;
@@ -44,7 +55,61 @@ public class MainActivityTest {
     }
 
     @Test
-    public void shouldStartScreenService() {
+    public void shouldStartEventService() {
+        ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
+        activity.mConnection = Mockito.mock(ServiceConnection.class);
 
+        controller.create().start();
+
+        serviceButton = (ToggleButton) activity.findViewById(R.id.start_service_btn);
+        serviceButton.performClick();
+
+        Intent serviceIntent = shadowActivity.getNextStartedService();
+
+        String expectedName = WakeService.class.getName();
+        String actualName = serviceIntent.getComponent().getClassName();
+
+        assertNotNull(actualName);
+        assertEquals(expectedName, actualName);
     }
+
+    @Test
+    public void shouldStopEventService() {
+        ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
+        activity.mConnection = Mockito.mock(ServiceConnection.class);
+
+        Mockito.when(svcUtilities.isServiceRunning(WakeService.class)).thenReturn(true);
+        controller.create().start();
+
+        serviceButton = (ToggleButton) activity.findViewById(R.id.start_service_btn);
+        serviceButton.performClick();
+
+        Intent serviceIntent = shadowActivity.getNextStoppedService();
+
+        String expectedName = WakeService.class.getName();
+        String actualName = serviceIntent.getComponent().getClassName();
+
+        assertNotNull(actualName);
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void buttonShouldReflectServiceState(){
+        ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
+        activity.mConnection = Mockito.mock(ServiceConnection.class);
+
+        Mockito.when(svcUtilities.isServiceRunning(WakeService.class)).thenReturn(false);
+        controller.create().start();
+
+        serviceButton = (ToggleButton) activity.findViewById(R.id.start_service_btn);
+        assertFalse(serviceButton.isChecked());
+
+        Mockito.when(svcUtilities.isServiceRunning(WakeService.class)).thenReturn(true);
+        controller.create().start();
+
+        serviceButton = (ToggleButton) activity.findViewById(R.id.start_service_btn);
+        Assert.assertTrue(serviceButton.isChecked());
+    }
+
+
 }
